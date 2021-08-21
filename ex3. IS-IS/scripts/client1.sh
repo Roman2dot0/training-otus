daa807db1c2a2e4672d7a74c36d6f515bd20cffb
Roman2dot0 <kud.rom.sh@gmail.com>
2021-08-21 18:05:11 +0300

    delete cumulus

--- ex3. IS-IS/scripts/client1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-net add bond bond0 bond slaves swp1-2
-net add bond bond0 ip address 10.1.0.100/24
-net add routing route 0.0.0.0/0 10.1.0.1
-net add routing route 0.0.0.0/0 10.1.0.2
-net pending
-net commit
-reboot
